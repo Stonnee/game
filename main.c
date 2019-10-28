20528abdae980e10bc7005a7e2d5b533222f5c3f
Axel <Coyere_a>
2019-10-28 10:03:06 +0100

    réglage du but et pt B

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 
 char source(int source)
 {
+
   char *bare = NULL;
 
   bare = malloc(sizeof(source) * source);
@@ -19,23 +20,25 @@ char source(int source)
   return *bare;
 }
 
-void but(char plateau[100][100], int x, int y, char **B)
+void but(char plateau[100][101], int x, int y, char **B)
 {
 
   plateau[x][y] = 'p';
-  
+ //B comme "but" = l'adresse de la localisation du but 
   *B = &plateau[35][13];
   
 }
 
 int main()
 {
+//maps
+ char plateau[100][101];
 
-  char plateau[100][100];
-  
+  //coordoner du but
   char *B = NULL;
 
   but(plateau, 35, 13, &B);
+///////////////////////////
 
   printf("%c", *B);  
 
