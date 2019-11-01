93adfaa52871b35952b7a889791f60c4121e20f7
Axel <Coyere_a>
2019-11-01 10:42:37 +0100

    iib

--- main.c
+++ main.c
@@ -21,21 +21,22 @@ int main()
 
   while ((toi[0] != fin[0]) || (toi[1] != fin[1]))
   {
-
+    afficherTableau(plateau, 100, 101);
     moove(plateau);
+    
     toi = crd(plateau, 'y');
-    printf("%d%d\n", toi[0], toi[1]);
 
-    printf("1%c\n", plateau[95][51]);
+   /*  printf("1%c\n", plateau[95][51]);
     printf("1%c\n", plateau[96][51]);
     printf("2%c\n", plateau[97][51]);
     printf("3%c\n", plateau[98][51]);
     printf("4%c\n", plateau[99][51]);
-    printf("5%c\n", plateau[100][51]);
+    printf("5%c\n", plateau[100][51]);*/
 
     printf("\n");
     printf("\n");
     printf("**********************************\n");
+    system("cls");
   }
 
   return 0;
