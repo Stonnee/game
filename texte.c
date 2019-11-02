3c5f8fc7ef96fd492c620d525b56b27919eee68f
Axel <Coyere_a>
2019-11-02 16:02:19 +0100

    ajout de la fonction txt, tableau 100 pour 101, sleep remplacer pas windows, 2eme fichier c

--- texte.c
+++ texte.c
@@ -1,5 +1,21 @@
 #include "bibi.h"
 
+void début()
+{
+    printf("mise à jour de l'invité de commande...\n\n");
+    windows(4000);
+    printf("ajout de l'extention ouverture des porte...\n\n\n\n");
+    windows(2000);
+    printf("altération des du system d'exploitation\n\n\n");
+    windows(600);
+    printf("ajout de la porte des lymbe...\n\n");
+    windows(2000);
+    printf("...\n\n\n\n\n");
+    windows(4000);
+    printf("ouverture...\n\n");
+    system("cls");
+}
+
 void txt(char *phrase, int vitesse)
 {
     char *test = phrase;
