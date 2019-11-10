c28ffc334db0750ed8b3a877dc9b7ad593105b22
Axel <Coyere_a>
2019-11-10 22:16:05 +0100

    hugo

--- main.c
+++ main.c
@@ -7,6 +7,8 @@ int main()
   Smonde *mape = malloc(sizeof(Smonde));
 
   fuck(mape->plateau);
+
+  debut();
    act1(mape);
  
  but(mape, 30,48);
@@ -19,10 +21,7 @@ int main()
   you(mape, 48, 4);
 
   but(mape, 30, 37);
-  for (int i = 0; i < 2; i++)
-  {
-     moove(mape);
-  }
+  
   txt("il semble que vous soyer tombe dans une crevasse,", 32);windows(500); txt("mais il y a aussi un inscription au sole.\n", 32);windows(500); txt("en appuyant sur la touche d'action je pourrais vous la lire.", 32);
   
  
